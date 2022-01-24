49c219cf5c1309c1a9f9d555b44b0b9119d74960
RaviojhaDp <ravi.ohja@dolphinwebsolution.com>
2022-01-24 12:11:21 +0530

    Luxmadein Plugin insurance mail after order status (Damaini Website)

---
+++ Luxmadein Plugin for Damiani/app/code/Luxmadein/Certificatodisostituzione/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Luxmadein_Certificatodisostituzione',
+    __DIR__
+);
